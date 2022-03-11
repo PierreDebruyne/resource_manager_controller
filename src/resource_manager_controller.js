24d3e7fc089c6c9848a5fb3ee754f31c64533486
Pierre DE BRUYNE <pierre0debruyne@gmail.com>
2022-03-11 16:58:11 +0100

    package type module

--- src/resource_manager_controller.js
+++ src/resource_manager_controller.js
@@ -1,62 +1,62 @@
-import axios from "axios";
-import {download_file} from "js_tools/tools";
+const axios = require("axios");
+const {download_file} = require("js_tools/tools");
 
-export default class {
+module.exports.ResourceManagerController = class {
     constructor(url, port) {
         this.url = "http://" + url + ":" + port;
-    }
-}
 
-export async function get_host(host_name) {
-    const resource_manager_url = this.url + "/resources/hosts/" + host_name;
-    try {
-        var {data: resource_manager} = await axios.get(resource_manager_url)
-        return resource_manager;
-    } catch (e) {
-        console.log(e.message)
-        throw {message: "Le propriétaire n'existe pas: " + host_name};
-    }
-}
+        async function get_host(host_name) {
+            const resource_manager_url = this.url + "/resources/hosts/" + host_name;
+            try {
+                var {data: resource_manager} = await axios.get(resource_manager_url)
+                return resource_manager;
+            } catch (e) {
+                console.log(e.message)
+                throw {message: "Le propriétaire n'existe pas: " + host_name};
+            }
+        }
 
-export async function get_type(host_name, type_name) {
-    const resource_manager_url = this.url + "/resources/hosts/" + host_name + "/types/" + type_name;
-    try {
-        var {data: resource_manager} = await axios.get(resource_manager_url)
-        return resource_manager;
-    } catch (e) {
-        console.log(e.message)
-        throw {message: "Le type n'existe pas: " + host_name + "/" + type_name};
-    }
-}
+        async function get_type(host_name, type_name) {
+            const resource_manager_url = this.url + "/resources/hosts/" + host_name + "/types/" + type_name;
+            try {
+                var {data: resource_manager} = await axios.get(resource_manager_url)
+                return resource_manager;
+            } catch (e) {
+                console.log(e.message)
+                throw {message: "Le type n'existe pas: " + host_name + "/" + type_name};
+            }
+        }
 
-export async function get_resource(host_name, type_name, resource_name) {
-    const resource_manager_url = this.url + "/resources/hosts/" + host_name + "/types/" + type_name + "/resources/" + resource_name
-    try {
-        var {data: resource_manager} = await axios.get(resource_manager_url)
-        return resource_manager;
-    } catch (e) {
-        console.log(e.message)
-        throw {message: "La ressource n'existe pas: " + host_name + "/" + type_name + "/" + resource_name};
-    }
-}
+        async function get_resource(host_name, type_name, resource_name) {
+            const resource_manager_url = this.url + "/resources/hosts/" + host_name + "/types/" + type_name + "/resources/" + resource_name
+            try {
+                var {data: resource_manager} = await axios.get(resource_manager_url)
+                return resource_manager;
+            } catch (e) {
+                console.log(e.message)
+                throw {message: "La ressource n'existe pas: " + host_name + "/" + type_name + "/" + resource_name};
+            }
+        }
 
-export async function get_release(host_name, type_name, resource_name, release_name) {
-    const resource_manager_url = this.url + "/resources/hosts/" + host_name + "/types/" + type_name + "/resources/" + resource_name + "/releases/" + release_name
-    try {
-        var {data: resource_manager} = await axios.get(resource_manager_url)
-        return resource_manager;
-    } catch (e) {
-        console.log(e.message)
-        throw {message: "La release n'existe pas: " + host_name + "/" + type_name + "/" + resource_name + "/" + release_name};
-    }
-}
+        async function get_release(host_name, type_name, resource_name, release_name) {
+            const resource_manager_url = this.url + "/resources/hosts/" + host_name + "/types/" + type_name + "/resources/" + resource_name + "/releases/" + release_name
+            try {
+                var {data: resource_manager} = await axios.get(resource_manager_url)
+                return resource_manager;
+            } catch (e) {
+                console.log(e.message)
+                throw {message: "La release n'existe pas: " + host_name + "/" + type_name + "/" + resource_name + "/" + release_name};
+            }
+        }
 
-export async function download_release(host_name, type_name, resource_name, release_name, out_path) {
-    const resource_manager_url = this.url + "/resources/hosts/" + host_name + "/types/" + type_name + "/resources/" + resource_name + "/releases/" + release_name
-    try {
-        await download_file(resource_manager_url + "/download", out_path);
-    } catch (e) {
-        console.log(e.message)
-        throw {message: "Impossible de télécharger la release: " + host_name + "/" + type_name + "/" + resource_name + "/" + release_name};
+        async function download_release(host_name, type_name, resource_name, release_name, out_path) {
+            const resource_manager_url = this.url + "/resources/hosts/" + host_name + "/types/" + type_name + "/resources/" + resource_name + "/releases/" + release_name
+            try {
+                await download_file(resource_manager_url + "/download", out_path);
+            } catch (e) {
+                console.log(e.message)
+                throw {message: "Impossible de télécharger la release: " + host_name + "/" + type_name + "/" + resource_name + "/" + release_name};
+            }
+        }
     }
 }
